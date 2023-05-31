735966a023dd45065bc8afdbe220dd5ef1573bb9
Otávio Gabriel <107057360+Otavioalv@users.noreply.github.com>
2023-05-31 14:06:45 -0400

    updates

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
-function Curriculo() {
-    var divImagens = document.getElementById('CurriculContainer');
+function show(id) {
+    var divImagens = document.getElementById(id);
     divImagens.classList.toggle('d-none');
+}
+
+
+var checkLike = true
+function like() {
+    var heartLike = document.querySelectorAll('.heartLike-1');
+
+    if(checkLike){
+        checkLike = false
+
+        heartLike.forEach((icon) =>  {
+            icon.classList.remove('bi-heart');
+            icon.classList.add('bi-heart-fill');
+        });
+    }
+    else{
+        checkLike = true
+
+        heartLike.forEach((icon) => {
+            icon.classList.remove('bi-heart-fill');
+            icon.classList.add('bi-heart');
+        });
+    }
+
+    console.log(checkLike)
 }
